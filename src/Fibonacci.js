8100336a5a38d5660cb9a70453129471b641435d
Matt Holland <hollandmatt@gmail.com>
2019-05-04 16:25:54 -0700

    Cleanup app scaffold, add a Fibonacci number generator module

---
+++ src/Fibonacci.js
@@ -0,0 +1,33 @@
+/**
+ * Generate an array with the first n entries in the Fibonacci sequence.
+ * 
+ * @param {*} n How many numbers to generate
+ */
+const getFirstNFibonacciNumbers = n => {
+  const fibonacciNumbers = [];
+
+  while (fibonacciNumbers.length < n) {
+    fibonacciNumbers.push(getNthFibonacciNumber(fibonacciNumbers.length))
+  }
+
+  return fibonacciNumbers;
+}
+
+/**
+ * Get the nth number in the Fibonacci sequence.
+ * 
+ * We're using tail recursion here to generate the numbers in O(n) time
+ * with O(n) space. 
+ * 
+ * @param {*} n Which Fibonacci number to generate
+ * @param {*} a f(n - 2)
+ * @param {*} b f(n - 1)
+ */
+const getNthFibonacciNumber = (n, a = 0, b = 1) => {
+  if (n === 0) return a;
+  if (n === 1) return b;
+
+  return getNthFibonacciNumber(n - 1, b, a + b); 
+}
+
+export { getFirstNFibonacciNumbers, getNthFibonacciNumber }
---
+++ src/Fibonacci.test.js
@@ -0,0 +1,23 @@
+import { getNthFibonacciNumber, getFirstNFibonacciNumbers } from './Fibonacci';
+import _ from 'lodash';
+
+describe('Fibonacci number generator', () => {
+  it('generates the correct amount of numbers', () => {
+    [1, 3, 5, 10, 50, 100].forEach(n => {
+      expect(getFirstNFibonacciNumbers(n).length).toBe(n);
+    });
+  });
+
+  it('generates the correct value', () => {
+    const fibs = {
+      0: 0,
+      1: 1,
+      5: 5,
+      10: 55
+    };
+    
+    _.keys(fibs).forEach(n => {
+      expect(getNthFibonacciNumber(Number(n))).toBe(fibs[n]);
+    });
+  });
+});
